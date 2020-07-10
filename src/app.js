c4cdebeba159a4a3d23929f51d3472874b62ab4c
Carrie Wang <carrie.g.wang@duke.edu>
2020-07-09 23:01:46 -0400

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 const { brotliDecompressSync } = require('zlib')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -96,6 +97,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('yeehaw')
+app.listen(port, () => {
+    console.log('server is up on port ' + port)
 })
